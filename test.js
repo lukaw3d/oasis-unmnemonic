9c505df7c5fa81da68b99671a6fd57949cec82b0
lukaw3d <lukajeran@oasislabs.com>
2022-02-22 01:10:24 +0100

    Test

--- test.js
+++ test.js
@@ -1,6 +1,11 @@
 // @ts-check
+const assert = require('assert')
 const { getPrivateKeys } = require('./getPrivateKeys.js')
+// const { hdKeyDerivation } = require('@zondax/ledger-substrate/dist/index')
 
 const privateKeys = getPrivateKeys('test test test test test test test test test test test test test test test test test test test test test test fun test')
 console.log('private keys')
 console.log(privateKeys.join('\n'))
+
+assert.equal(privateKeys[0], 'mEKxtuhFGRSBmjF/D3VCSCbmKOrNaabdEpgKvRATXlg=')
+assert.equal(privateKeys[1], 'oHumu4kyxdYbIlOZnItEHFnNQndBGCsEQaynNBMTXlg=')
